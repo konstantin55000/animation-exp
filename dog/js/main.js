fc90ed40934bd0153e59801f4017870240967775
konstantin55000 <pkonstantin55000@gmail.com>
2020-10-29 16:29:38 +0200

    added tail speed

--- dog/js/main.js
+++ dog/js/main.js
@@ -1,7 +1,7 @@
  window.addEventListener('DOMContentLoaded', () => {
      console.log(document.getElementById("container"));
      gsap.to('#dog-head', 1, { y: 5, rotation: 14, ease: "circ.out", repeat: 1, yoyo: true });
-     gsap.to('#tail', 0.5, { x: 2, repeat: -1, yoyo: true });
+     gsap.to('#tail', 0.4, { x: 2, repeat: -1, yoyo: true });
      gsap.to('#front-legs', 0.8, { x: 4, rotation: -25, ease: "circ.out" });
      gsap.to('#back-legs', 0.8, { x: 1.5, rotation: -10, ease: "circ.out", yoyo: true, repeat: 1 });
  });
