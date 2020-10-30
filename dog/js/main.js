01389c85076f3154aee4846c84c198d0da5904b9
konstantin55000 <pkonstantin55000@gmail.com>
2020-10-30 16:26:25 +0200

    changed animation on hover

--- dog/js/main.js
+++ dog/js/main.js
@@ -1,18 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log(document.getElementById("container"));
 
-    document.querySelector('#btn-head').addEventListener('mouseover', () => {
-        gsap.to('#dog-head', 1, { y: 2, rotation: 2, ease: "circ.out", repeat: 1, yoyo: true });
+    document.querySelector('#btn-head').addEventListener('mouseenter', () => {
+        gsap.to('#dog-head', 0.8, { y: 2, rotation: 2, ease: "circ.out", repeat: 1, yoyo: true });
     });
 
-    document.querySelector('#btn-tail').addEventListener('mouseover', () => {
-        gsap.to('#tail', 0.5, { x: 1, rotation: 1, repeat: 2, yoyo: true });
+    document.querySelector('#btn-tail').addEventListener('mouseenter', (evt) => {
+        gsap.to('#tail', 0.5, { x: 1, rotation: 1.5, ease: "circ.inOut", yoyo: true, repeat: 1 });
     });
 
-    document.querySelector('#btn-front-legs').addEventListener('mouseover', () => {
+    document.querySelector('#btn-front-legs').addEventListener('mouseenter', () => {
         gsap.to('#front-leg-left', 0.8, { x: 2, rotation: -4, yoyo: true, repeat: 1, ease: "circ.out" });
         gsap.to('#front-leg-right', 0.8, { x: 2, rotation: -1, yoyo: true, repeat: 1, ease: "circ.out" });
-    });
 
+    });
 
 });
